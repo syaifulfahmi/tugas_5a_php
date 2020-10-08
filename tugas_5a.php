b0c8c12e0556bf01106e0a62ec1aa3335d4506ca
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-08 11:25:07 +0700

    Add files via upload

---
+++ tugas_5a.php
@@ -0,0 +1,37 @@
+<?php
+
+	echo "<h3>Tabel Logika 1111</h3>";
+	echo "<table border='1'>
+		<tr>
+			<td>Input True</td>
+			<td>Input 2</td>
+			<td>AND</td>
+			<td>OR</td> 
+		</tr>
+		<tr>
+			<td>false</td>
+			<td>false</td>
+			<td>".printf(false && false)."</td>
+			<td>".printf(false || false)."</td>
+		</tr>
+		<tr>
+			<td>false</td>
+			<td>true</td>
+			<td>".printf(false && true)."</td>
+			<td>".printf(false || true)."</td>
+		</tr>
+		<tr>
+			<td>true</td>
+			<td>false</td>
+			<td>".printf(true && false)."</td>
+			<td>".printf(true || false)."</td>
+		</tr>
+		<tr>
+			<td>true</td>
+			<td>true</td>
+			<td>".printf(true && true)."</td>
+			<td>".printf(true || true)."</td>
+		</tr>
+		
+	</table>"
+?>
